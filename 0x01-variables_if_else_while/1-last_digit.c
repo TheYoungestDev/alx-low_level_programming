8052bd7299097a4d78275f41b999abaaeff58129
TheYoungestDev <iamabdullahitijani@gmail.com>
2023-10-26 16:37:50 -0700

    Fixed update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - Entry point
+ *
+ * This function assigns a random number to the variable 'n' and
+ * each time it is executed. Complete the source code in order to
+ * print the last digit of the number stored in the variable 'n'.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     int n;
