90a6494af0a27df0f5e55e55f0a1bff87e0d0e77
TheYoungestDev <iamabdullahitijani@gmail.com>
2023-10-26 07:39:21 -0700

    started next one

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,1 +1,23 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * This function assigns a random number to the variable 'n' each time it is executed,
+ * print the last digit of the number stored in the variable 'n'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if 
+	return (0);
+}
