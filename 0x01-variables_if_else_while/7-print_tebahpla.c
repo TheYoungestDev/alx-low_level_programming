da07b9f63a57372b033545c9cea9bd01ee441cd0
TheYoungestDev <iamabdullahitijani@gmail.com>
2023-10-28 08:39:54 -0700

    7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,8 +3,19 @@
 /**
  * main - Entry point
  *
- * This program prints all single digit numbers of
- * base 10 starting from 0, followed by a new line.
+ * This program prints the lowercase alphabet in reverse, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
+
+int main(void)
+{	char letter;
+
+	for (letter ='z'; letter >= 'a'; letter--)
+	{	putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
