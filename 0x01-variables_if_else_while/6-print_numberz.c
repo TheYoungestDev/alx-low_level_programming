194f0e1eaa5cbe6fc08dafbf74a1c0de7ec5225a
TheYoungestDev <iamabdullahitijani@gmail.com>
2023-10-28 07:58:50 -0700

    6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,26 +3,26 @@
 /**
  * main - Entry point
  *
- * This program prints all single digit numbers of 
+ * This program prints all single digit numbers of
  * base 10 starting from 0, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 
-int main(void) {
-    putchar(48);  /* ASCII value for '0'*/
-    putchar(49);  /*  ASCII value for '1'*/
-    putchar(50);  /*  ASCII value for '2'*/
-    putchar(51);  /*  ASCII value for '3'*/
-    putchar(52);  /*  ASCII value for '4'*/
-    putchar(53);  /*  ASCII value for '5'*/
-    putchar(54);  /*  ASCII value for '6'*/
-    putchar(55);  /*  ASCII value for '7'*/
-    putchar(56);  /*  ASCII value for '8' */
-    putchar(57);  /* ASCII value for  '9' */
+int main(void)
+{
+	putchar(48);  /* ASCII value for '0'*/
+	putchar(49);  /*  ASCII value for '1'*/
+	putchar(50);  /*  ASCII value for '2'*/
+	putchar(51);  /*  ASCII value for '3'*/
+	putchar(52);  /*  ASCII value for '4'*/
+	putchar(53);  /*  ASCII value for '5'*/
+	putchar(54);  /*  ASCII value for '6'*/
+	putchar(55);  /*  ASCII value for '7'*/
+	putchar(56);  /*  ASCII value for '8' */
+	putchar(57);  /* ASCII value for  '9' */
+	putchar('\n');
 
-    putchar('\n'); /*  New line */
-
-    return 0;
+	return (0);
 }
 
