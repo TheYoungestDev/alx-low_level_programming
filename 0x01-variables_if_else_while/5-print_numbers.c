9fc5d2ddd7e8dc76e274f887a0c682b0f8029b28
TheYoungestDev <iamabdullahitijani@gmail.com>
2023-10-28 07:41:41 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This program prints the alphabet in lowercase,
+ * then in uppercase followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+
+	putchar('\n');
+
+	return 0;
+}
