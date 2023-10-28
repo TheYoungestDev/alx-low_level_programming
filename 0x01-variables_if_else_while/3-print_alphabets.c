152f31e02e5587af6b1b512bec39d6c601aa9460
TheYoungestDev <iamabdullahitijani@gmail.com>
2023-10-28 07:20:12 -0700

    3 fixed

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,18 +14,16 @@ int main(void)
 	char uppercase = 'A';
 
 	while (lowercase <= 'z')
-    {
-	    putchar(lowercase);
-	    lowercase++;
-    }
-
-    while (uppercase <= 'Z')
-    {
-	    putchar(uppercase);
-	    uppercase++;
-    }
-
-    putchar('\n');
-    return (0);
+	{
+		putchar(lowercase);
+		lowercase++;
+	}
+	while (uppercase <= 'Z')
+	{
+		putchar(uppercase);
+		uppercase++;
+	}
+	putchar('\n');
+	return (0);
 }
 
