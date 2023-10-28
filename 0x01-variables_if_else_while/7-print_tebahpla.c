6a237db6ec470d6eaba7a6e674ec39fa4c1e2a64
TheYoungestDev <iamabdullahitijani@gmail.com>
2023-10-28 08:45:39 -0700

    7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * This program prints the lowercase alphabet 
+ *This program prints the lowercase alphabet 
  * in reverse, followed by a new line.
  *
  * Return: Always 0 (Success)
