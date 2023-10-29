ad7da6cb055f708e785c540d59189b8f51edbb16
TheYoungestDev <iamabdullahitijani@gmail.com>
2023-10-29 14:26:40 -0700

    added

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,27 +8,29 @@
  * Return: Always 0 (Success)
  */
 
+
 int main(void)
 {
-	for (int digit1 = 0; digit1 <= 7; digit1++)
+	int digit1, digit2, digit3;
+	
+	for (digit1 = 0; digit1 <= 7; digit1++)
 	{
-		for (int digit2 = digit1 + 1; digit2 <= 8; digit2++)
+        for (digit2 = digit1 + 1; digit2 <= 8; digit2++)
+	{
+		for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
 		{
-			for (int digit3 = digit2 + 1; digit3 <= 9; digit3++)
-			{
-				putchar(digit1 + '0');
-				putchar(digit2 + '0');
-				putchar(digit3 + '0');
-
-				if (digit1 < 7)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-
+		    putchar(digit1 + '0');
+		    putchar(digit2 + '0');
+		    putchar(digit3 + '0');
+		    
+		    if (digit1 < 7)
+		    {
+			    putchar(',');
+			    putchar(' ');
+                }
+            }
+        }
+    }
 	putchar('\n');
 	return (0);
 }
