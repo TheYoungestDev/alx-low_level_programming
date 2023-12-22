48e6f1533adcb0f368bc987daccc43a2190685b8
TheYoungestDev <iamabdullahitijani@gmail.com>
2023-12-21 22:47:36 -0800

    Updated

---
+++ 0x03-debugging/positive-keep.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * This function assigns a random number to the variable 'n' and
+ * prints whether the number is positive, zero, or negative.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) /*This function assigns a random number*/
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        /* your code goes there */
+        if (n > 0)
+        {
+                printf("%d is positive\n", n);
+        }
+        else if (n == 0)
+        {
+                printf("%d is zero\n", n);
+        } else
+        {
+                printf("%d is negative\n", n);
+        }
+
+        return (0);
+}
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,34 +1,25 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-/* more headers goes there */
+#include "main.h"  // Include the header file to declare the function
 
 /**
- * main - Entry point
- *
- * This function assigns a random number to the variable 'n' and
- * prints whether the number is positive, zero, or negative.
- *
- * Return: Always 0 (Success)
+ * positive_or_negative - Prints whether a number is positive, zero, or negative.
+ * @i: The number to be checked.
  */
-int main(void) /*This function assigns a random number*/
+void positive_or_negative(int i)
 {
-        int n;
-
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        /* your code goes there */
-        if (n > 0)
-        {
-                printf("%d is positive\n", n);
-        }
-        else if (n == 0)
-        {
-                printf("%d is zero\n", n);
-        } else
-        {
-                printf("%d is negative\n", n);
-        }
-
-        return (0);
+    if (i > 0)
+    {
+        printf("%d is positive\n", i);
+    }
+    else if (i == 0)
+    {
+        printf("%d is zero\n", i);
+    }
+    else
+    {
+        printf("%d is negative\n", i);
+    }
 }
+
